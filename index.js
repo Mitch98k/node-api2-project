d16498fb9e6f2221fb25d93bccfab0bd8c049f7b
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-05-13 22:19:56 -0600

    Added start script

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // require your server and launch it here
+require('dotenv').config();
+
 const server = require('./api/server');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log(`\n*** server listening on port ${port} ***\n`);
